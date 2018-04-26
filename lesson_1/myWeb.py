395adc46b74de292d825d9038693dda2e3ac95a8
achuDk <achudk@qq.com>
2018-04-26 19:14:41 +0800

    2

--- lesson_1/myWeb.py
+++ lesson_1/myWeb.py
@@ -1,11 +1,12 @@
 from wsgiref.simple_server import make_server
-
+import time
 
 def route():
     url_pattern = {
         ("/",index),
         ("/login",login),
         ("/regist",regist),
+        ("/show_time",show_time)
     }
     return url_pattern
 
@@ -15,6 +16,9 @@ def index(req):
     return data
 
 def login(req):
+    # print(req)
+    print(req["QUERY_STRING"])
+
     f = open('login.html','rb')
     data = f.read()
     return data
@@ -23,6 +27,18 @@ def regist(req):
     f = open('regist.html','rb')
     data = f.read()
     return data
+
+def show_time(req):
+    times = time.ctime()
+    # 第一种方法
+    # return ('<h1>时间： [ %s ]</h1>' %str(times)).encode('gbk')
+
+    # 第二种方法
+    f = open('show_time.html','rb')
+    data = f.read()
+    data = data.decode('utf8')
+    data = data.replace('{{time}}',str(times))
+    return data.encode('utf8')
 
 def application(environ,start_response):
     # print("========>",environ)
