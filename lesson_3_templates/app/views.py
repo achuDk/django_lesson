e7c42bd0fdfe26ea4d08100b88fdbf3f2e868847
achuDk <achudk@qq.com>
2018-05-02 18:35:09 +0800

    10

---
+++ lesson_3_templates/app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import time,datetime
+# Create your views here.
+
+
+class Animal():
+    def __init__(self,name,sex):
+        self.name = name
+        self.sex = sex
+
+def show_time(request):
+    time = datetime.datetime.now()
+    list = ['a',1,{"name":"alex"}]
+    dict = {"name":"yuan","age":21}
+    obj = Animal('tom','male')
+    filter_test = 'hello world !'
+    tag_a = '<a href=''>click</a>'
+    return render(request,'show_time.html',locals())
+
+
+def index(request):
+    list = ['alex','yuan','egon']
+    dict = {"name": "yuan", "age": 21}
+    return render(request,'index.html',locals())
