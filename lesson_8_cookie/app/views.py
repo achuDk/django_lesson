e5a8a5d70d5293b3a8db120052e7fed120bd7c80
achuDk <achudk@qq.com>
2018-05-08 18:02:53 +0800

    18

--- lesson_8_cookie/app/views.py
+++ lesson_8_cookie/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-
+import datetime
 # Create your views here.
 
 def login(request):
@@ -10,11 +10,12 @@ def login(request):
         pwd = request.POST.get("pwd")
         if user == "yuan" and pwd == "123":
             ret = redirect('/index/')
-            # ret.set_cookie("user",user)
+            # 过期时间为3天
+            # ret.set_cookie("user",user,max_age=172800,expires=datetime.datetime.utcnow()+datetime.datetime.timedelta(days=3))
 
             request.session['if_login'] = True
             request.session['user'] = user
-
+            request.session.set_expiry(172800)
             return ret
     return render(request,'login.html')
 
@@ -22,11 +23,12 @@ def login(request):
 def index(request):
     # ret = request.COOKIES.get("user",None)
 
-    if request.session.get("if_login",None):
-        ret = request.session.get("user",None)
-        print("ret3",ret)
-        if ret:
-            user = ret
-            return render(request,'index.html',locals())
+    ret = request.session.get("user", None)
+
+    print("ret3", ret)
+    if request.session.get("if_login",None) and ret:
+    # if ret:
+        user = ret
+        return render(request,'index.html',locals())
     else:
         return redirect('/login/')
